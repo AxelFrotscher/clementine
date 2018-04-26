3db39b3c1808693c896e3b492693c07d24c969c1
axel <afrotscher@ikp.tu-darmstadt.de>
2018-04-26 14:43:54 +0200

    Everything's working again. New TTree method (external class) implemented. IC consistency check done.

--- src/histograms.cpp
+++ src/histograms.cpp
@@ -15,11 +15,10 @@ const bool closeness(const vector<double> &d, double sigma = 0.1){
     return sigma * mean > sqrt(sq_sum / d.size());
 }
 
-void makepid(TTreeReader &datree, TFile &output, const vector<bool> &goodevents){
-    datree.Restart();
+void makepid(treereader &tree, TFile &output, const vector<bool> &goodevents){
     // 5 beams, 2incoming, 3outgoing
-    TTreeReaderArray<double> aoqevt (datree, "BigRIPSBeam.aoq");
-    TTreeReaderArray<double> zetevt (datree, "BigRIPSBeam.zet");
+    vector <string> keys{"BigRIPSBeam.aoq", "BigRIPSBeam.zet"};
+    tree.setloopkeys(keys);
 
     vector<TH2D> PID {
         TH2D("pidinc", "PID Incoming F3-F7",  300,2.45,2.9, 200,25,45),
@@ -42,27 +41,29 @@ void makepid(TTreeReader &datree, TFile &output, const vector<bool> &goodevents)
 
     int eventcounter =0;
     vector<vector<double>> valinc;     // Store temporary beam values
-    while(datree.Next()){
+    while(tree.singleloop()){
         if(goodevents.at(eventcounter)){
         //Loop over all elements in the tree
-        valinc.push_back({aoqevt[0],aoqevt[1],aoqevt[2]});
-        valinc.push_back({zetevt[0],zetevt[1],zetevt[2]});
+        valinc.push_back({tree.BigRIPSBeam_aoq[0],tree.BigRIPSBeam_aoq[1],
+                          tree.BigRIPSBeam_aoq[2]});
+        valinc.push_back({tree.BigRIPSBeam_zet[0],tree.BigRIPSBeam_zet[1],
+                          tree.BigRIPSBeam_zet[2]});
         if(closeness(valinc.at(0)) && closeness(valinc.at(1))){
             // Cut Particles that have variating aoq or zet
-            PID.at(0).Fill(aoqevt[0],zetevt[0]);
+            PID.at(0).Fill(tree.BigRIPSBeam_aoq[0],tree.BigRIPSBeam_zet[0]);
         }
 
-        valinc.push_back({aoqevt[3],aoqevt[4]});
-        valinc.push_back({zetevt[3], zetevt[4]});
+        valinc.push_back({tree.BigRIPSBeam_aoq[3],tree.BigRIPSBeam_aoq[4]});
+        valinc.push_back({tree.BigRIPSBeam_zet[3], tree.BigRIPSBeam_zet[4]});
         if(closeness(valinc.at(2)) && closeness(valinc.at(3))){
-            PID.at(1).Fill(aoqevt[4],zetevt[4]);
+            PID.at(1).Fill(tree.BigRIPSBeam_aoq[4],tree.BigRIPSBeam_zet[4]);
         }
         valinc.clear();
 
-        if((pow(1./cutval.at(2)*(aoqevt[0]-cutval.at(0)),2) +
-           pow(1/cutval.at(3)*(zetevt[0]- cutval.at(1)),2)) <1){
-            PID.at(2).Fill(aoqevt[0],zetevt[0]);
-            PID.at(3).Fill(aoqevt[4],zetevt[4]);
+        if((pow(1./cutval.at(2)*(tree.BigRIPSBeam_aoq[0]-cutval.at(0)),2) +
+           pow(1/cutval.at(3)*(tree.BigRIPSBeam_zet[0]- cutval.at(1)),2)) <1){
+            PID.at(2).Fill(tree.BigRIPSBeam_aoq[0],tree.BigRIPSBeam_zet[0]);
+            PID.at(3).Fill(tree.BigRIPSBeam_aoq[4],tree.BigRIPSBeam_zet[4]);
         }}
         eventcounter++;
     }
@@ -72,38 +73,36 @@ void makepid(TTreeReader &datree, TFile &output, const vector<bool> &goodevents)
     output.cd("");
 }
 
-void plastics(TTreeReader &datree, TFile &output, vector<bool> &goodevents){
+void plastics(treereader &tree, TFile &output, vector<bool> &goodevents){
     // This function aims to rebuild the trigger from Q1*Q2 F7plastic
     // therefore we set a limit on sqrt(Q1*Q2) to suppress random noise
     printf("Now beginning with reconstruction of plastic scintillators ...\n");
     const int numplastic = 4;
     const int F7pos = 1;
-    const int threshhold = 450; // random noise suppresion value 
-    datree.Restart();
-    TTreeReaderArray<int> plasticQleft (datree, "BigRIPSPlastic.fQLRaw");
-    TTreeReaderArray<int> plasticQright(datree, "BigRIPSPlastic.fQRRaw");    
-    TTreeReaderArray<int> plastictleft (datree, "BigRIPSPlastic.fTLRaw");
-    TTreeReaderArray<int> plastictright(datree, "BigRIPSPlastic.fTRRaw");    
-    TTreeReaderArray<int> plasticnum   (datree, "BigRIPSPlastic.fpl");
+    const int threshhold = 450; // random noise suppresion value
 
+    vector<string> keys{"BigRIPSPlastic.fQLRaw", "BigRIPSPlastic.fQRRaw",
+                        "BigRIPSPlastic.fTLRaw", "BigRIPSPlastic.fTRRaw",
+                        "BigRIPSPlastic.fpl"};
+    tree.setloopkeys(keys);
     // generate output diagrams
     vector<TH2D> qcorr2D;  // Charge Correlation between 1 & 2
     vector<TH1D> qcorr;    // deposited charge at detector
     vector<TH2D> tqcorr2D; // Charge-Time correlation
     
     vector<vector<string>> arrayname, arraytitle;
-    datree.Next(); // Get first event for invovled focal planes
+    tree.singleloop(); // Get first event for invovled focal planes
     for(uint i=0; i<numplastic; i++){
         arrayname.push_back(vector<string>{
-            "f7pltrigQ."  + to_string(plasticnum[i]), 
-            "PlasticQ2D." + to_string(plasticnum[i]),
-            "tQcorr."     + to_string(plasticnum[i])
+            "f7pltrigQ."  + to_string(tree.BigRIPSPlastic_fpl[i]),
+            "PlasticQ2D." + to_string(tree.BigRIPSPlastic_fpl[i]),
+            "tQcorr."     + to_string(tree.BigRIPSPlastic_fpl[i])
         });
 
         arraytitle.push_back(vector<string>{
-            "Charge deposition by Plastic F"     + to_string(plasticnum[i]),
-            "Charge distribution by Plastic F"   + to_string(plasticnum[i]),
-            "Charge Ratio/time difference Pl. F" + to_string(plasticnum[i])
+            "Charge deposition by Plastic F"     + to_string(tree.BigRIPSPlastic_fpl[i]),
+            "Charge distribution by Plastic F"   + to_string(tree.BigRIPSPlastic_fpl[i]),
+            "Charge Ratio/time difference Pl. F" + to_string(tree.BigRIPSPlastic_fpl[i])
         });
 
         qcorr2D.emplace_back(TH2D(arrayname.at(i).at(1).c_str(),
@@ -131,22 +130,31 @@ void plastics(TTreeReader &datree, TFile &output, vector<bool> &goodevents){
     vector<vector<int>> range{{400,500},{450,750},{213,350},{260,1310}};
     int i=0; // counting variable
 
-    while(datree.Next()){
-        if(sqrt(plasticQleft[F7pos]*plasticQright[F7pos])> threshhold)
+    while(tree.singleloop()){
+        if(sqrt(tree.BigRIPSPlastic_fQLRaw[F7pos]*
+                tree.BigRIPSPlastic_fQRRaw[F7pos])> threshhold)
         for(uint i=0; i<numplastic; i++){
-            if((plasticQleft[i] >0 )&& (plasticQright[i] >0)){
-                qcorr2D.at(i).Fill(plasticQleft[i],plasticQright[i]);
-                qcorr.at(i).Fill(sqrt(plasticQleft[i]*plasticQright[i]));
-                if((plastictleft[i] >0) && (plastictright[i] >0) ){
-                    tqcorr2D.at(i).Fill(plastictleft[i]-plastictright[i],
-                        TMath::Log(plasticQleft[i]/(double)plasticQright[i]));
+            if((tree.BigRIPSPlastic_fQLRaw[i] >0 )&&
+               (tree.BigRIPSPlastic_fQRRaw[i] >0)){
+                qcorr2D.at(i).Fill(tree.BigRIPSPlastic_fQLRaw[i],
+                                   tree.BigRIPSPlastic_fQRRaw[i]);
+                qcorr.at(i).Fill(sqrt(tree.BigRIPSPlastic_fQLRaw[i]*
+                                      tree.BigRIPSPlastic_fQRRaw[i]));
+                if((tree.BigRIPSPlastic_fTLRaw[i] >0) &&
+                   (tree.BigRIPSPlastic_fTRRaw[i] >0) ){
+                    tqcorr2D.at(i).Fill(tree.BigRIPSPlastic_fTLRaw[i]-
+                                        tree.BigRIPSPlastic_fTRRaw[i],
+                        TMath::Log(tree.BigRIPSPlastic_fQLRaw[i]/
+                                   (double)tree.BigRIPSPlastic_fQRRaw[i]));
                 }
             }
         }
         for(int j=0;j<numplastic;j++){
             goodevents[i] =goodevents[i]*
-                       (sqrt(plasticQleft[j]*plasticQright[j]) > range[j][0])*
-                       (sqrt(plasticQleft[j]*plasticQright[j]) < range[j][1]);
+                       (sqrt(tree.BigRIPSPlastic_fQLRaw[j]*
+                             tree.BigRIPSPlastic_fQRRaw[j]) > range[j][0])*
+                       (sqrt(tree.BigRIPSPlastic_fQLRaw[j]*
+                             tree.BigRIPSPlastic_fQRRaw[j]) < range[j][1]);
         }
         i++;
     }
@@ -168,21 +176,17 @@ void plastics(TTreeReader &datree, TFile &output, vector<bool> &goodevents){
     printf("Finished Writing plastic histograms! \n");
 }
 
-void ppacs(TTreeReader &datree, TFile &output, vector<bool> &goodevents){
+void ppacs(treereader &tree, TFile &output, vector<bool> &goodevents){
     printf("Now beginning with reconstruction of the ppac's ...\n");
     const int numplane = 36;
     const int pl11position = 3; //Plastic at F11 is fourth in array
-    datree.Restart();
+    vector<string> keys{"BigRIPSPlastic.fTime", "BigRIPSPPAC.fFiredX",
+                        "BigRIPSPPAC.fFiredY", "BigRIPSPPAC.fTSumX",
+                        "BigRIPSPPAC.fTDiffX", "BigRIPSPPAC.fTSumY",
+                        "BigRIPSPPAC.fTDiffY", "BigRIPSPPAC.name"};
 
-    TTreeReaderArray<double> triggerpl11(datree,  "BigRIPSPlastic.fTime");
-    TTreeReaderArray<bool>   bigripsx   (datree,  "BigRIPSPPAC.fFiredX");
-    TTreeReaderArray<bool>   bigripsy   (datree,  "BigRIPSPPAC.fFiredY");
-    TTreeReaderArray<double> bigxsum    (datree,  "BigRIPSPPAC.fTSumX");
-    TTreeReaderArray<double> bigxdiff   (datree,  "BigRIPSPPAC.fTDiffX");
-    TTreeReaderArray<double> bigysum    (datree,  "BigRIPSPPAC.fTSumY");
-    TTreeReaderArray<double> bigydiff   (datree,  "BigRIPSPPAC.fTDiffY");
-    
-    TTreeReaderArray<TString> bripsname(datree, "BigRIPSPPAC.name");
+    tree.setloopkeys(keys);
+
     //36 Values per Array (Event)
     TH1D effPPACX("effPPACX", "Efficiency of PPAC X", numplane, 0, numplane);
     TH1D effPPACY("effPPACY", "Efficiency of PPAC Y", numplane, 0, numplane);
@@ -220,16 +224,16 @@ void ppacs(TTreeReader &datree, TFile &output, vector<bool> &goodevents){
     effPPACY.GetYaxis()->SetTitle("PPACY(x)/Pplastic(F11)");
 
     //Fill names with first event
-    //datree.Next();
+    tree.singleloop();
     //cout << "T STRING: " << bripsname.operator[](0).Data() << endl;
-/*    for(int i=1; i<=numplane; i++){
-        effPPACX.GetXaxis()->SetBinLabel(i, bripsname[i-1].Data());
-        effPPACY.GetXaxis()->SetBinLabel(i, bripsname[i-1].Data());
-    }*/
+    for(int i=1; i<=numplane; i++){
+        effPPACX.GetXaxis()->SetBinLabel(i, tree.BigRIPSPPAC_name[i-1].Data());
+        effPPACY.GetXaxis()->SetBinLabel(i, tree.BigRIPSPPAC_name[i-1].Data());
+    }
     
     // 0=X, 1=Y  || 0...35 Plane No.
     uint total = 0;
-    uint fulltotal =0;  //Skipping first event for name generation
+    uint fulltotal =1;  //Skipping first event for name generation
 
     // Define Planes of Importance corresponding to fpl 3,5,7,8,9,11
     const vector<vector<int>> ppacplane{{4,5,6,7},{9,10,11,12},{14,15,16,17},
@@ -247,23 +251,23 @@ void ppacs(TTreeReader &datree, TFile &output, vector<bool> &goodevents){
     vector<bool> temptruth(4, true);
 
     printf("Filling in Plastics ... \n");
-    while(datree.Next()){
+    while(tree.singleloop()){
         // Get Efficiency relative to the last Plastic
-        if (triggerpl11[pl11position] >0){
+        if (tree.BigRIPSPlastic_fTime[pl11position] >0){
             for(int i=1; i<=numplane; i++){
                 //ripsvec.at(0).at(i) 
-                if(bigripsx[i-1]) 
+                if(tree.BigRIPSPPAC_fFiredX[i-1])
                     effPPACX.SetBinContent(i, effPPACX.GetBinContent(i)+1);
-                if(bigripsy[i-1]) 
+                if(tree.BigRIPSPPAC_fFiredY[i-1])
                     effPPACY.SetBinContent(i, effPPACY.GetBinContent(i)+1);
             }
             total++;
         }
         for(int i=0; i<numplane;i++){
-            sumdiffppac.at(0).at(0).Fill(i,bigxsum[i]);
-            sumdiffppac.at(0).at(1).Fill(i,bigxdiff[i]);
-            sumdiffppac.at(1).at(0).Fill(i,bigysum[i]);
-            sumdiffppac.at(1).at(1).Fill(i,bigydiff[i]);
+            sumdiffppac.at(0).at(0).Fill(i,tree.BigRIPSPPAC_fTSumX[i]);
+            sumdiffppac.at(0).at(1).Fill(i,tree.BigRIPSPPAC_fTDiffX[i]);
+            sumdiffppac.at(1).at(0).Fill(i,tree.BigRIPSPPAC_fTSumY[i]);
+            sumdiffppac.at(1).at(1).Fill(i,tree.BigRIPSPPAC_fTDiffY[i]);
         }
 
         // Make Cut conditions: each focal Plane needs to have one valid entry
@@ -271,10 +275,10 @@ void ppacs(TTreeReader &datree, TFile &output, vector<bool> &goodevents){
         for(uint i =0; i<ppacplane.size();i++){
             for(uint j=0; j<4; j++){ // Loop over all 4 PPAC's per Focal Plane
             temptruth.at(j) =
-                (bigxsum[ppacplane.at(i).at(j)] > ppacrange.at(i).at(j).at(0) &&
-                 bigxsum[ppacplane.at(i).at(j)] < ppacrange.at(i).at(j).at(1) &&
-                 bigysum[ppacplane.at(i).at(j)] > ppacrange.at(i).at(j).at(2) &&
-                 bigysum[ppacplane.at(i).at(j)] < ppacrange.at(i).at(j).at(3));
+                (tree.BigRIPSPPAC_fTSumX[ppacplane.at(i).at(j)] > ppacrange.at(i).at(j).at(0) &&
+                 tree.BigRIPSPPAC_fTSumX[ppacplane.at(i).at(j)] < ppacrange.at(i).at(j).at(1) &&
+                 tree.BigRIPSPPAC_fTSumY[ppacplane.at(i).at(j)] > ppacrange.at(i).at(j).at(2) &&
+                 tree.BigRIPSPPAC_fTSumY[ppacplane.at(i).at(j)] < ppacrange.at(i).at(j).at(3));
             }
             // Recursively check each focal plane for at least 1 good Signal
             goodevents.at(fulltotal) = goodevents.at(fulltotal)*
@@ -310,7 +314,7 @@ void ppacs(TTreeReader &datree, TFile &output, vector<bool> &goodevents){
     printf("Finished Writing PPAC histogram!\n");
 }
 
-void ionisationchamber(TTreeReader &datree, TFile &output,
+void ionisationchamber(treereader &alt2dtree, TFile &output,
                        vector<bool> &goodevents) {
     // This method aims to control the Ionisation Chamber values
     // therefore only certain events are accepted
@@ -345,9 +349,10 @@ void ionisationchamber(TTreeReader &datree, TFile &output,
     uint totalcounter =0;
 
     while(alt2dtree.singleloop()){
-        if(alt2dtree.BigRIPSIC_nhitchannel[0]*
-           alt2dtree.BigRIPSIC_nhitchannel[1] <16) //Require 4 hits in each IC
+        if((alt2dtree.BigRIPSIC_nhitchannel[0]*
+           alt2dtree.BigRIPSIC_nhitchannel[1]) <16){ //Require 4 hits in each IC
             goodevents.at(totalcounter) = false;
+        }
         if((alt2dtree.BigRIPSIC_fADC[0][0] <0) ||
            (alt2dtree.BigRIPSIC_fADC[1][0] <0)) continue;
         for(int i=0; i<(numchannel-1);i++){
@@ -372,19 +377,12 @@ void ionisationchamber(TTreeReader &datree, TFile &output,
     output.cd("");
 }
 
-void highordercorrection(TTreeReader &datree, TFile &output){
+void highordercorrection(treereader &alt2dtree, TFile &output){
     // This method aims to determine the higher-order corrections
     // for the matrix elements
     printf("Now beginning with higher order corrections ...\n");
-    datree.Restart();
-
-    vector<TTreeReaderValue<double>> culprit{
-        TTreeReaderValue<double> (datree,  "F3X"),
-        TTreeReaderValue<double> (datree,  "F3A"),
-        TTreeReaderValue<double> (datree,  "F5X"),
-        TTreeReaderValue<double> (datree,  "F5A")};
-
-    TTreeReaderArray<double> aoqevt (datree, "BigRIPSBeam.aoq");
+    vector<string> keys{"F3X","F3A", "F5X", "F5A", "BigRIPSBeam.aoq"};
+    alt2dtree.setloopkeys(keys);
     
     const int beam = 0; // Evaluate Beam F3-7 (1st element)
     const vector<string> arrname = { "DepF3X","DepF3A","DepF5X", "DepF5A"};
@@ -402,11 +400,11 @@ void highordercorrection(TTreeReader &datree, TFile &output){
       culpritdiag.back().GetYaxis()->SetTitle(arrname.at(i).c_str());
     }
 
-    while(datree.Next()){
-        for(uint i=0; i<arrname.size();i++){
-            printf("Starting Higherorder Loop...%f\n", aoqevt[0]);
-            culpritdiag.at(i).Fill(aoqevt[beam], *culprit.at(i));
-        }
+    while(alt2dtree.singleloop()){
+        culpritdiag.at(0).Fill(alt2dtree.BigRIPSBeam_aoq[beam], alt2dtree.F3X);
+        culpritdiag.at(1).Fill(alt2dtree.BigRIPSBeam_aoq[beam], alt2dtree.F3A);
+        culpritdiag.at(2).Fill(alt2dtree.BigRIPSBeam_aoq[beam], alt2dtree.F5X);
+        culpritdiag.at(3).Fill(alt2dtree.BigRIPSBeam_aoq[beam], alt2dtree.F5A);
     }
     //gDirec
     output.Delete("Corrections");
@@ -418,8 +416,8 @@ void highordercorrection(TTreeReader &datree, TFile &output){
 }
 
 void makehistograms(const string input){
-    //TFile inputfile(input.c_str());
-    //if(!inputfile.IsOpen()) __throw_invalid_argument("Input file not valid.");
+    TFile inputfile(input.c_str());
+    if(!inputfile.IsOpen()) __throw_invalid_argument("Input file not valid.");
 
     treereader alt2dtree; // Opens the input file...
 
@@ -428,8 +426,8 @@ void makehistograms(const string input){
     if(!outputfile.IsOpen()) __throw_invalid_argument("Output file not valid");
 
     vector<bool> options{
-        false,  // Plastics
-        false,  // ppac
+        true,  // Plastics
+        true,  // ppac
         true,  // Ionisationchamber
         true,  // highordercorrection
         true   // makepid
@@ -442,18 +440,18 @@ void makehistograms(const string input){
     TTreeReader mytreereader("tree", &inputfile);
 
     // Rebuild F7 Trigger combined charge threshhold
-    if(options.at(0)) plastics(mytreereader, outputfile, goodevents);
+    if(options.at(0)) plastics(alt2dtree, outputfile, goodevents);
     
     // Cut the PPAC's
-    if(options.at(1)) ppacs(mytreereader, outputfile, goodevents);
+    if(options.at(1)) ppacs(alt2dtree, outputfile, goodevents);
 
-    if(options.at(2)) ionisationchamber(mytreereader, outputfile, goodevents);
+    if(options.at(2)) ionisationchamber(alt2dtree, outputfile, goodevents);
     printf("Finished with PPAC Consistency checks\n");
     // Get Corrections
-    if(options.at(3)) highordercorrection(mytreereader, outputfile);
+    if(options.at(3)) highordercorrection(alt2dtree, outputfile);
 
     // Get Z vs. A/Q
-    if(options.at(4)) makepid(mytreereader, outputfile, goodevents);
+    if(options.at(4)) makepid(alt2dtree, outputfile, goodevents);
     printf("Made PID histograms in %s\n", output.c_str());
 
     cout << "Runs has " <<accumulate(goodevents.begin(),goodevents.end(),0)
